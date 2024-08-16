207c145121d7684e967e140fcff1290dc02493fe
rjumabekov <rjumabekov>
2024-08-16 17:21:00 +0600

    CombinationSum

--- src/algorithms/medium/CombinationSum.java
+++ src/algorithms/medium/CombinationSum.java
@@ -1,36 +1,42 @@
 package algorithms.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSum {
 
     private static List<List<Integer>> combinationSum(int[] can, int target) {
+        Arrays.sort(can);
         List<List<Integer>> lists = new ArrayList<>();
-        int i = 0;
-        while (i != can.length) {
-            int num = can[i];
-            if (num > target)
-                continue;
-            if (num == target) {
-                lists.add(List.of(num));
-                continue;
-            }
-
-            if ((target % num) == 0) {
-                int j = target / num;
-                List<Integer> integers = new ArrayList<>();
-                for (int k = 0; k < j; k++) {
-                    integers.add(j);
-                }
-                lists.add(integers);
-                i++;
-            }
-            if (num < target){
-            }
-
-        }
-
+        lists.add(new ArrayList<>());
+        sum(can, target, 0, lists, 0, 0);
         return lists;
+    }
+
+    private static int sum(int[] can, int target, int arrayIndex, List<List<Integer>> lists, int currentListIndex, int lastSum) {
+        List<Integer> integers = lists.get(currentListIndex);
+        for (int i = arrayIndex; i < can.length; i++) {
+            if (lastSum < target) {
+                lastSum += can[i];
+                integers.add(can[i]);
+                int sum = sum(can, target, i, lists, currentListIndex, lastSum);
+                if (sum > target) {
+                    integers.remove(can[i]);
+                }
+            } else if (lastSum > target) {
+                lastSum += can[i];
+                lastSum = sum(can, target, i + 1, lists, currentListIndex, lastSum);
+            } else {
+                ArrayList<Integer> newArray = new ArrayList<>();
+                lists.add(newArray);
+                lastSum = sum(can, target, i + 1, lists, currentListIndex + 1, 0);
+            }
+        }
+        return lastSum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(combinationSum(new int[]{2, 3, 6, 7}, 7));
     }
 }
