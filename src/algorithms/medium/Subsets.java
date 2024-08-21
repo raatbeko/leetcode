d80a7e2f5138864f5c5c389cfccc846f71410e8c
rjumabekov <rjumabekov>
2024-08-21 19:58:45 +0600

    Subsets

--- src/algorithms/medium/Subsets.java
+++ src/algorithms/medium/Subsets.java
@@ -10,21 +10,24 @@ public class Subsets {
         Arrays.sort(nums);
         List<List<Integer>> lists = new ArrayList<>();
         lists.add(new ArrayList<>());
-        subset(nums, 0, lists);
+        subset(nums, 0, lists, new ArrayList<>());
         return lists;
     }
 
-    private static void subset(int[] nums, int curren, List<List<Integer>> lists) {
-        if (nums.length == curren)
+    private static void subset(int[] nums, int next, List<List<Integer>> lists, List<Integer> integers) {
+        if (nums.length == next)
             return;
 
-        for (int i = curren; i < nums.length; i++) {
+        for (int i = next; i < nums.length; i++) {
+            integers.add(nums[i]);
+            lists.add(new ArrayList<>(integers));
 
+            subset(nums, i + 1, lists, new ArrayList<>(integers));
+            integers.remove(integers.size() - 1);
         }
-        subset(nums, curren + 1, lists);
     }
 
     public static void main(String[] args) {
-        System.out.println(subsets(new int[]{1, 2}));
+        System.out.println(subsets(new int[]{}));
     }
 }
