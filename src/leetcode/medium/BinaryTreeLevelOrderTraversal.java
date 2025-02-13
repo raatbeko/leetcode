6ac32582b2d605688173574e8cedc7731f1c1c51
raatbek <ratbekjumabekov@gmail.com>
2025-02-13 09:19:22 +0600

    add tasks

---
+++ src/leetcode/medium/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,41 @@
+package leetcode.medium;
+
+import structure.binaryTree.TreeNode;
+
+import java.util.*;
+
+public class BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> link = new ArrayList<>();
+        if (root == null) return link;
+        Queue<TreeNode> nodes = new ArrayDeque<>();
+        nodes.add(root);
+        link.add(List.of(root.val));
+
+        Queue<TreeNode> child = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+
+        while (!nodes.isEmpty()) {
+            TreeNode peek = nodes.poll();
+            if (peek.left != null) {
+                child.add(peek.left);
+                list.add(peek.left.val);
+            }
+            if (peek.right != null) {
+                child.add(peek.right);
+                list.add(peek.right.val);
+            }
+            if (nodes.isEmpty()) {
+                nodes = child;
+                child = new ArrayDeque<>();
+                if (!list.isEmpty()) {
+                    link.add(list);
+                    list = new ArrayList<>();
+                }
+            }
+        }
+        Collections.reverse(link);
+        return link;
+    }o
+}
